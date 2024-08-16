52b9011d5fadc9a4f4e7f176cea191d1d042d9e7
schoolfromage <slarbuckle@hotmail.com>
2024-08-16 16:56:39 -0700

    added house collision

--- PlayableScenes/PlacementController.gd
+++ PlayableScenes/PlacementController.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var selectedObject:Node = null
+var validPlace:bool = false
 
 @onready var Houses = {
 	0: preload("res://Objects/Houses/base_house.tscn")
@@ -16,18 +17,28 @@ func makeSelection(selection:int):
 func _input(event):
 	# Mouse in viewport coordinates.
 	if selectedObject != null:
-		if event is InputEventMouseButton:
+		if event is InputEventMouseButton and validPlace == true:
 			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 				remove_child(selectedObject)
 				get_tree().root.add_child(selectedObject)
 				selectedObject.set_process(true)
 				selectedObject.position = get_viewport().get_camera_2d().get_global_mouse_position()
+				selectedObject.modulate = Color.WHITE
 				selectedObject = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#position = get_viewport().get_mouse_position()+get_viewport().get_camera_2d().position
-	position = get_global_mouse_position()
+	if selectedObject != null:
+		position = get_global_mouse_position()
+		#TODO: restrict position to grid
+		var overlaps = selectedObject.get_node('Area2D').get_overlapping_areas()
+		if overlaps.size() == 0:
+			selectedObject.modulate = Color(0,1,0,0.5)#blue
+			validPlace = true
+		else:
+			selectedObject.modulate = Color(1,0,0,0.5)#red
+			validPlace = false
 	
 func _draw():
 	draw_circle(Vector2.ZERO, 36, Color(0,1,0,0.5))
