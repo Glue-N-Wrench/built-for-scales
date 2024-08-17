c6464a6372e0be2036cbe04ed9bb18cb2a097688
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-08-16 20:37:23 -0400

    ui sfx

--- UI/MainMenu/BackBtn.gd
+++ UI/MainMenu/BackBtn.gd
@@ -1,4 +1,6 @@
 extends Button
+@onready var ui_sound2 = $"../../../../audio/ui2"
 
 func _on_pressed():
 	get_parent().hide()
+	ui_sound2.play()
--- UI/MainMenu/Credits Btn.gd
+++ UI/MainMenu/Credits Btn.gd
@@ -1,8 +1,12 @@
 extends Button
+@onready var ui_sound1 = $"../../audio/ui1"
+
 
 func _ready():
 	$Popup.hide()
+	
 
 func _on_pressed():
 	$Popup.hide()#hide to reset window position
 	$Popup.show()
+	ui_sound1.play()
