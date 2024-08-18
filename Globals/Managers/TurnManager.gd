da7b2d1488791daa0b67cb0999cf97cdea146aa6
schoolfromage <slarbuckle@hotmail.com>
2024-08-17 23:36:37 -0700

    made game over screen

--- Globals/Managers/TurnManager.gd
+++ Globals/Managers/TurnManager.gd
@@ -7,15 +7,30 @@ extends Node
 
 var dayCount = 0
 signal updateDay # a signal for the UI
-const roundTime = 20 #seconds in a round
+const roundTime = 5 #seconds in a round
 var roundTimer = 0
 const weekTime = 7 #rounds in a week
 var weekTimer = 0
 
+signal updateGameOverTimer
+var gameOverCount = 10#seconds till game over
+var gameOverTimer = 0
+
 func _process(delta):
+	#==manage the day==
 	roundTimer -= delta
 	if roundTimer < 0:
 		roundTimer = roundTime
 		dayCount += 1
 		FishManager.makeNewFishBatch(4)
 		updateDay.emit()
+	#==manage the game over
+	if FishManager.getTotalHomeless() > FishManager.maxHomeless:
+		gameOverTimer += delta
+		updateGameOverTimer.emit()
+		if gameOverTimer > gameOverCount:
+			$"/root/MainLevel/Camera2D/GameOverScreen".visible=true
+			get_tree().paused = true
+	elif gameOverTimer > 0:
+		gameOverTimer -= delta
+		updateGameOverTimer.emit()
