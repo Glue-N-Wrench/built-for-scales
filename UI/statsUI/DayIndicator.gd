04673dfdf254739ab7e1a20dbf603275b61617df
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2024-08-19 16:51:23 -0700

    Squashed commit of the following:
    commit b1ed9948e641b785f0855911b0cbade5bf2af5d5
    Author: schoolfromage <slarbuckle@hotmail.com>
    Date:   Mon Aug 19 16:46:50 2024 -0700
    
        moved house assets
    
    commit 38dcd36d3dc5c6b29b7676163ab7d38e2d8e734c
    Merge: 0850e41 244b37f
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 19:43:48 2024 -0400
    
        Merge pull request #49 from Glue-N-Wrench/gameplay-sfx-implementaion
    
        Gameplay sfx implementaion
    
    commit 244b37f891ed78350d67cd39c37c38c71ebfb14f
    Merge: cf00b1c 0850e41
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 19:42:50 2024 -0400
    
        Merge branch 'project' into gameplay-sfx-implementaion
    
    commit cf00b1c13df8b47754f5e16f0a3687e1a4fa4871
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 19:42:31 2024 -0400
    
        bomb, fix music player bug, fog horn sfx
    
    commit 0850e4123e458527fe817def104f75f3d7af9416
    Merge: 8dda176 f597ab8
    Author: schoolfromage <slarbuckle@hotmail.com>
    Date:   Mon Aug 19 16:28:57 2024 -0700
    
        Merge branch 'project' of https://github.com/Glue-N-Wrench/GMTK2024 into project
    
    commit 8dda176590e1c1848e6aeff91cfc847f2d9930cd
    Author: schoolfromage <slarbuckle@hotmail.com>
    Date:   Mon Aug 19 16:28:42 2024 -0700
    
        fix fish bug
    
    commit 8dca451dfc7c270fe4895797091c5dc43e9c1422
    Merge: b30c97d f597ab8
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 19:16:28 2024 -0400
    
        Merge branch 'project' into gameplay-sfx-implementaion
    
    commit b30c97d9bc1910d41424c2a3ef7ce881b0d75c10
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 19:15:59 2024 -0400
    
        bomb sfx import
    
    commit f597ab8a852539111168ed1808db2f4577ade8e4
    Merge: ba6edc2 1d1d4ba
    Author: SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
    Date:   Mon Aug 19 16:12:43 2024 -0700
    
        Merge pull request #48 from Glue-N-Wrench/45-make-bomb-logic-and-use
    
        45 make bomb logic and use
    
    commit ba6edc2cff0efa3c356d26aaa7bb0f9ef16bc851
    Author: schoolfromage <slarbuckle@hotmail.com>
    Date:   Mon Aug 19 16:11:44 2024 -0700
    
        first pollish bomb and camera
    
    commit 815980b341118942f0c681688e2e8d3665542b40
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 19:09:40 2024 -0400
    
        new day sfx
    
    commit e565fbaca164ee0c65fd4e64154a850a0ac6e125
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 18:51:53 2024 -0400
    
        fish enter and house placement audio
    
    commit 2e8e06d73e25b9ff3f94ac530378d793d054214e
    Merge: 028fbc8 4414dde
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 18:44:21 2024 -0400
    
        Merge branch 'project' into gameplay-sfx-implementaion
    
    commit 028fbc8eca62cd71dac9ece91e67ff37ad28aa01
    Author: Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
    Date:   Mon Aug 19 18:39:25 2024 -0400
    
        add place house sfx
    
    commit 4414ddeb27289a85afc83cae5402bda59a62066c
    Merge: aacb90d 828c42a
    Author: SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
    Date:   Mon Aug 19 15:25:56 2024 -0700
    
        Merge pull request #47 from Glue-N-Wrench/45-make-bomb-logic-and-use
    
        Bombs are done!!!

--- UI/statsUI/DayIndicator.gd
+++ UI/statsUI/DayIndicator.gd
@@ -1,10 +1,13 @@
 extends TextureRect
 
+@onready var foghorn_sfx = $"../../foghorn sfx"
+
 func _ready():
 	TurnManager.updateDay.connect(_on_day_update)
 
 func _on_day_update():
 	$Label.text = "Day\n"+str(TurnManager.dayCount)
+	foghorn_sfx.play()
 
 func _process(delta):
 	$Sun.rotation = -(TurnManager.roundTimer / TurnManager.roundTime) * TAU
