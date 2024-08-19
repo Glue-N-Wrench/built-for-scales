815980b341118942f0c681688e2e8d3665542b40
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-08-19 19:09:40 -0400

    new day sfx

--- UI/statsUI/DayIndicator.gd
+++ UI/statsUI/DayIndicator.gd
@@ -1,10 +1,12 @@
 extends TextureRect
+@onready var new_day_sfx = $"../../new day sfx"
 
 func _ready():
 	TurnManager.updateDay.connect(_on_day_update)
 
 func _on_day_update():
 	$Label.text = "Day\n"+str(TurnManager.dayCount)
+	new_day_sfx.play()
 
 func _process(delta):
 	$Sun.rotation = -(TurnManager.roundTimer / TurnManager.roundTime) * TAU
