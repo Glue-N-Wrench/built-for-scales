828c42aa22b787b2792cd7f9c7a55bcc21af33d0
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2024-08-19 15:25:29 -0700

    Bombs are done!!!

---
+++ UI/statsUI/bomb.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var bomb = preload("res://Objects/bomb.tscn")
+@onready var tilemap: TileMap = $"/root/MainLevel/Tilemaps/Breakables"
+var distToGround:int = 64
+var offset:Vector2 = Vector2(0,0)
+
+func Explode():
+	$AffectedArea.visible = false
+	$BombAnimator.play("Explode")
+
+func onPlace():
+	Explode()
+
+
+func _on_bomb_animator_frame_changed():
+	if $BombAnimator.frame == 6 and $BombAnimator.animation == "Explode":
+		var TL = tilemap.local_to_map(position - Vector2(80,80))
+		var BR = tilemap.local_to_map(position + Vector2(80,80))
+		var destroyedCells = []
+		for x in range(TL.x, BR.x):
+			for y in range(TL.y, BR.y):
+				tilemap.erase_cell(0, Vector2(x,y))
+				tilemap.erase_cell(1, Vector2(x,y))
+				destroyedCells.append(Vector2(x,y))
+		
+		tilemap.set_cells_terrain_connect(0, destroyedCells, 0, -1, false)
+		tilemap.set_cells_terrain_connect(1, destroyedCells, 0, -1, false)
+	if $BombAnimator.frame == 12:
+		queue_free()
