a8eda3c7bff4e0b4f0f5b313d8ec81a5e6aed23c
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2024-08-18 21:10:11 -0700

    Just need to implement proper rewards now, base system is done

--- UI/weeklyRewards.gd
+++ UI/weeklyRewards.gd
@@ -1,36 +1,35 @@
 extends Control
 
 var rng = RandomNumberGenerator.new()
-#should store [Name, Amount, NodePath, PictureNodePath]
+#should store [Name, Amount, PictureNodePath]
 #TODO REPLACE PLACEHOLERS WITH PROPER NODES
-var rewardArray = [["Bombs", $"Placeholder", 3, $"res://assets/bomb.png"]]
-var options
+var rewardArray = [["Bombs", 3, $"res://assets/bomb.png"]]
+var options = [[$"HBoxContainer/Option 1", ""], [$"HBoxContainer/Option 2", ""], [$"HBoxContainer/Option 3", ""]]
 
 func displayWeeklyRewards():
-	get_tree().paused
+	get_tree().paused = true
 	var weeklyRewardsUI = $"/root/MainLevel/Camera2D/WeeklyRewards"
-	visible=true
-	options[0] = $"HBoxContainer/Option 1"
-	options[1] = $"HBoxContainer/Option 2"
-	options[2] = $"HBoxContainer/Option 3"
+	generateWeeklyOptions()
+	visible = true
 
 func generateWeeklyOptions():
+	#This SHOULD display 3 bombs for all three options & store the bomb node in the options array
 	for x in 3:
 		var reward = rng.randi_range(0,22)
-		#TODO REPLACE PLACEHOLDER [0]s WITH reward
-		#both first index [0]s here are placeholders
-		
-		#this SHOULD make an options text say "3 Bombs"
-		options[x].text = rewardArray[0][2] + rewardArray[0][0]
-		#this SHOULD make an options Icon bomb.png
-		options[x].icon = rewardArray[0][3]
-		
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	
-	pass # Replace with function body.
+		#TODO REPLACE PLACEHOLDER FIRST INDEX [0]s WITH reward
+		options[x][0].text = rewardArray[0][1] + rewardArray[0][0]
+		options[x][0].icon = rewardArray[0][2]
+		options[x][1] = rewardArray[0][0]
 
+func giveReward(choice):
+	#TODO MAKE LOGIC FOR EACH REWARD
+	print("givingReward" + choice)
+	match choice:
+		"Bombs":
+			pass
+			#TODO GIVE PLAYER BOMBS HERE
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func choseOption(optionNumber):
+	giveReward(options[optionNumber][1])
+	get_tree().paused = false
+	visible = false
