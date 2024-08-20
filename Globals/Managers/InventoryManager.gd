623a5bce9c08d52214647ce11c9094bb5b95ee63
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2024-08-19 23:28:23 -0700

    final house import

--- Globals/Managers/InventoryManager.gd
+++ Globals/Managers/InventoryManager.gd
@@ -11,7 +11,7 @@ var Buildings:Array[HouseInfo] = [
 		3, #Amount
 	),
 	HouseInfo.new(
-		"Alpha House",
+		"Alpha House A",
 		"2x2 house for tiny anchovies",
 		2,
 		preload("res://assets/Houses/house_2x2_a.png"),
@@ -20,38 +20,38 @@ var Buildings:Array[HouseInfo] = [
 	),
 	HouseInfo.new(
 		"Beta House A",
-		"2x2 house for medium chad",
-		2,
+		"2x2 house for medium chads",
+		4,
 		preload("res://assets/Houses/house_2x2_b.png"),
 		preload("res://Objects/Houses/Objects/house_b.tscn"),
 		2,
 	),
 	HouseInfo.new(
-		"3x2 Charley House",
+		"Charley House",
 		"3x2 house for big catfish",
-		5,
+		10,
 		preload("res://assets/Houses/house_3x2_a.png"),
 		preload("res://Objects/Houses/Objects/big_house_a.tscn"),
 		1,
 	),
 	HouseInfo.new(
-		"3x2 Delta House", #Name
+		"Delta House", #Name
 		"3x2 house for big catfish", #Description
-		5, #Chance Higher = more common
+		10, #Chance Higher = more common
 		preload("res://assets/Houses/house_3x2_b.png"), #Picture
 		preload("res://Objects/Houses/Objects/big_house_b.tscn"), #scene
 		1, #Amount
 	),
 		HouseInfo.new(
-		"2x2 Beta House B", #Name
-		"2x2 house for medium chad", #Description
-		2, #Chance Higher = more common
+		"Beta House B", #Name
+		"2x2 house for medium chads", #Description
+		4, #Chance Higher = more common
 		preload("res://assets/Houses/house_2x2_b.png"), #Picture
 		preload("res://Objects/Houses/Objects/house_b.tscn"), #scene
 		2, #Amount
 	),
 	HouseInfo.new(
-		"1x1 Foxtrot House A", #Name
+		"Foxtrot House A", #Name
 		"1x1 house for tiny anchovies", #Description
 		1, #Chance Higher = more common
 		preload("res://assets/Houses/house_1x1_a.png"), #Picture
@@ -59,7 +59,7 @@ var Buildings:Array[HouseInfo] = [
 		2, #Amount
 	),
 		HouseInfo.new(
-		"1x1 Foxtrot House B", #Name
+		"Foxtrot House B", #Name
 		"1x1 house for tiny anchovies", #Description
 		1, #Chance Higher = more common
 		preload("res://assets/Houses/house_1x1_b.png"), #Picture
@@ -67,7 +67,7 @@ var Buildings:Array[HouseInfo] = [
 		2, #Amount
 	),
 	HouseInfo.new(
-		"1x1 Foxtrot House C", #Name
+		"Foxtrot House C", #Name
 		"1x1 house for tiny anchovies", #Description
 		1, #Chance Higher = more common
 		preload("res://assets/Houses/house_1x1_c.png"), #Picture
@@ -75,7 +75,7 @@ var Buildings:Array[HouseInfo] = [
 		2, #Amount
 	),
 	HouseInfo.new(
-		"1x1 Foxtrot House D", #Name
+		"Foxtrot House D", #Name
 		"1x1 house for tiny anchovies", #Description
 		1, #Chance Higher = more common
 		preload("res://assets/Houses/house_1x1_d.png"), #Picture
@@ -83,21 +83,149 @@ var Buildings:Array[HouseInfo] = [
 		2, #Amount
 	),
 	HouseInfo.new(
-		"1x2 Golf House", #Name
+		"Golf House A", #Name
 		"1x2 house for medium chads", #Description
-		2, #Chance Higher = more common
+		4, #Chance Higher = more common
 		preload("res://assets/Houses/house_1x2_a.png"), #Picture
 		preload("res://Objects/Houses/Objects/house_g.tscn"), #scene
 		1, #Amount
 	),
-	#HouseInfo.new(
-		#"", #Name
-		#"", #Description
-		#1, #Chance Higher = more common
-		#preload(), #Picture
-		#preload(), #scene
-		#1, #Amount
-	#),
+		HouseInfo.new(
+		"Golf House B", #Name
+		"1x2 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_1x2_b.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_h.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Hotel House A", #Name
+		"1x3 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_1x3_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_i.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Hotel House B", #Name
+		"1x3 house for medium chads", #Description
+		4, #Chance Higher = more common
+		preload("res://assets/Houses/house_1x3_b.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_j.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"India House A", #Name
+		"2x1 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x1_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_k.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"India House B", #Name
+		"2x1 house for medium chads", #Description
+		4, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x1_b.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_L.tscn"), #scene
+		1, #Amount
+	),
+		HouseInfo.new(
+		"Beta House C", #Name
+		"2x2 house for medium chads", #Description
+		4, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x2_b.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_m.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Alpha House B", #Name
+		"2x2 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x2_c.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_n.tscn"), #scene
+		1, #Amount
+	),
+		HouseInfo.new(
+		"Juliett House A", #Name
+		"1 + 2x1 house for medium chads", #Description
+		4, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x2_DL_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_o.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Juliett House B", #Name
+		"1 + 2x1 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x2_DR_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_p.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Kilo House A", #Name
+		"2x1 + 1 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x2_UL_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_q.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Lima House A", #Name
+		"3x1 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_3x1_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_r.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Lima House B", #Name
+		"3x1 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_3x1_b.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_s.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Mike House", #Name
+		"3x1 + 1 house for medium chads", #Description
+		4, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x3T_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_t.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"November House", #Name
+		"2x1 + 1x3 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_2x4_UL_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_u.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Oscar House", #Name
+		"1x2 + 2x1 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_3x2_DL_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_v.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Papa House", #Name
+		"3x2 + 1 house for big catfish", #Description
+		20, #Chance Higher = more common
+		preload("res://assets/Houses/house_3x3_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_w.tscn"), #scene
+		1, #Amount
+	),
+	HouseInfo.new(
+		"Quebec House", #Name
+		"2x1 + 1 + 2x1 house for tiny anchovies", #Description
+		2, #Chance Higher = more common
+		preload("res://assets/Houses/house_3x2_c_a.png"), #Picture
+		preload("res://Objects/Houses/Objects/house_x.tscn"), #scene
+		1, #Amount
+	)
 ]
 
 signal inventoryUpdated # a new item, or an item was removed
