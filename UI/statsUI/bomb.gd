92fb627d3705881ce271d058eabb12fb509c8c93
technofromage <53595974+technofromage@users.noreply.github.com>
2024-12-06 10:52:03 -0800

    Fixed house supports
    I made a test house scene to diagnose problems that occur on-place

--- UI/statsUI/bomb.gd
+++ UI/statsUI/bomb.gd
@@ -35,18 +35,18 @@ func _on_bomb_animator_frame_changed():
 		#Effects
 		bomb_sfx.play()
 		$"Rock Particle Emitter".ActiveEmitter()
-		
-		var TL = tilemap.get_child(0).local_to_map(position - Vector2(80,80))
-		var BR = tilemap.get_child(0).local_to_map(position + Vector2(96,96))
-		var destroyedCells = []
-		for x in range(TL.x, BR.x):
-			for y in range(TL.y, BR.y):
-				tilemap.get_child(0).erase_cell(Vector2(x,y))
-				tilemap.get_child(1).erase_cell(Vector2(x,y))
-				destroyedCells.append(Vector2(x,y))
-		tilemap.get_child(0).set_cells_terrain_connect(destroyedCells, 0, -1, false)
-		tilemap.get_child(1).set_cells_terrain_connect(destroyedCells, 0, -1, false)
-		for house in houseOverlaps:
-			house.get_parent().destroy()
+		if tilemap:
+			var TL = tilemap.get_child(0).local_to_map(position - Vector2(80,80))
+			var BR = tilemap.get_child(0).local_to_map(position + Vector2(96,96))
+			var destroyedCells = []
+			for x in range(TL.x, BR.x):
+				for y in range(TL.y, BR.y):
+					tilemap.get_child(0).erase_cell(Vector2(x,y))
+					tilemap.get_child(1).erase_cell(Vector2(x,y))
+					destroyedCells.append(Vector2(x,y))
+			tilemap.get_child(0).set_cells_terrain_connect(destroyedCells, 0, -1, false)
+			tilemap.get_child(1).set_cells_terrain_connect(destroyedCells, 0, -1, false)
+			for house in houseOverlaps:
+				house.get_parent().destroy()
 	if $BombAnimator.frame == 12:
 		queue_free()
