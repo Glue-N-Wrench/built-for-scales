af7a0e2b673a815d61198dc7b6ecd5cdaf5bc2ce
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-12-10 19:39:43 -0500

    basic panels open/close sfx

--- UI/Options/OptionsMenu.gd
+++ UI/Options/OptionsMenu.gd
@@ -5,10 +5,11 @@ var selectedSoundOptions = OptionsMan.soundSettings.duplicate(true)
 const soundControl = preload("res://UI/Options/sound_control.tscn")
 var selectedControlOptions = OptionsMan.controlSettings.duplicate(true)
 const controlControl = preload("res://UI/Options/controls_control.tscn")
-@onready var open_panel_sfx = $"/root/MainAudio/open panel sfx"
+@onready var ui_open_option_sfx: AudioStreamPlayer2D = $"../../../UI Open Panel SFX"
+@onready var ui_close_panel_sfx: AudioStreamPlayer2D = $"../../../UI Close Panel SFX"
+
 @onready var soundPreviewSFX = $"../../../SoundPreviewSFX"
 
-@onready var open_panel_sfx____options = $"open panel sfx  - options"
 
 func _ready():
 	#==build sound options ==
@@ -55,14 +56,15 @@ func _on_apply_but_pressed():
 	OptionsMan.applySettings()
 
 func _on_close_but_pressed():
+	ui_close_panel_sfx.play()
 	resetOptions()
 	hide()
-	open_panel_sfx____options.play()
+	
 
 func _on_option_btn_pressed():
 	#this needs to be connect to a signal from outside the menu
 	show()
-	open_panel_sfx.play()
+	ui_open_option_sfx.play()
 
 func _on_close_requested():
 	resetOptions()
