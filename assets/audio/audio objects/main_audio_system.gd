b580e1cb2a0c2343dfef9f358ac0a5226169200b
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-12-12 20:33:19 -0500

    set up code for seperation of game vs main menu msuci

--- assets/audio/audio objects/main_audio_system.gd
+++ assets/audio/audio objects/main_audio_system.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var main_menu_music: AudioStreamPlayer = $"main menu music"
+@onready var gameplay_music: AudioStreamPlayer2D = $"gameplay music"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +12,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
+func play_game_music():
+	gameplay_music.play()
+	
+func stop_game_music():
+	gameplay_music.stop()
+	
 func play_music():
 	main_menu_music.play()
+	
 func stop_music():
 	main_menu_music.stop()
