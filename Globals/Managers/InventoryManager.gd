555178a4acf823ec2be45a7c2546de128245eef1
schoolfromage <slarbuckle@hotmail.com>
2025-02-02 17:16:18 -0800

    added houses to rotation; made windows build

--- Globals/Managers/InventoryManager.gd
+++ Globals/Managers/InventoryManager.gd
@@ -5,10 +5,10 @@ signal inventoryCountUpdated # a change in count without a change in inventory i
 var inventory = {
 	#buildingID: count
 	0:3,
-	#20:2,
+	20:2,
 	31:2,
 	32:3,
-	#5:1,
+	5:1,
 }
 
 func reset():
